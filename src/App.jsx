de6d59a80bfd624f5bfcea8fc4785043a994c508
Chanatinart K <chanatinart.k@gmail.com>
2023-08-24 01:23:10 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import styles from "./style";
+import "./App.css";
+
+import {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+} from "./components/index";
+
+function App() {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>NavBar</div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>Hero</div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>State</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
