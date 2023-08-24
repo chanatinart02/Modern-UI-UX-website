aab5f9ebbc87d88db93e2e5dad94c11a157b9fd2
Chanatinart K <chanatinart.k@gmail.com>
2023-08-24 16:01:06 +0700

    add Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import styles from "./style";
-import "./App.css";
 
 import {
   Navbar,
@@ -18,15 +17,28 @@ function App() {
   return (
     <div className="bg-primary w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-        <div className={`${styles.boxWidth}`}>NavBar</div>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
       </div>
 
       <div className={`bg-primary ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>Hero</div>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
       </div>
 
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>State</div>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
       </div>
     </div>
   );
