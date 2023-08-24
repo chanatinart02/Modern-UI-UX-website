88747638411e496833c23c5ebcfa7347cb25c931
Chanatinart K <chanatinart.k@gmail.com>
2023-08-24 18:25:06 +0700

    add Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,12 @@
-import React from "react";
-
 import GetStarted from "./GetStarted";
 import styles from "../style";
 import { discount, robot } from "../assets";
 
 const Hero = () => (
   <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
-    <div
-      className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
-    >
+    {/* Content Left side */}
+    <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+      {/* 20% DISCOUNT*/}
       <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
         <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
         <p className={`${styles.paragraph} ml-2`}>
@@ -16,17 +14,41 @@ const Hero = () => (
           <span className="text-white">1 MONTH</span> ACCOUNT
         </p>
       </div>
-
+      {/* Heading */}
       <div className="flex flex-row justify-between items-center w-full">
         <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px]">
           The Next <br className="sm:block hidden" />{" "}
-          <span className="text-gradient">Generation</span> <br />
-          Payment Method.
+          <span className="text-gradient">Generation</span>
         </h1>
-        <div className="ss:flex hidden md:mr-4 mr-0">
+        {/* Get started Btn */}
+        <div className="ss:flex hidden md:ml-4 mr-0">
           <GetStarted />
         </div>
       </div>
+
+      <h1 className=" font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full">
+        Payment Method.
+      </h1>
+
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Our team of experts uses a methodology to identify the credit cards most
+        likely to fit your needs. We examine annual percentage rates, annual
+        fees.
+      </p>
+    </div>
+
+    {/* Image Right Side */}
+    <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+      <img src={robot} alt="robot" className="w-[100%] h -[100%] z-[5]" />
+      {/* Gradient BG */}
+      <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
+      <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
+      <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
+    </div>
+
+    {/* Mobile screen show Get started Btn here  */}
+    <div className={`ss:hidden ${styles.flexCenter}`}>
+      <GetStarted />
     </div>
   </section>
 );
