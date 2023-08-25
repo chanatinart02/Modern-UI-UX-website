162c5c04d2e70e5a0face7bef5d8985537c838e9
Chanatinart K <chanatinart.k@gmail.com>
2023-08-25 12:45:38 +0700

    deploy gh

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ import {
 function App() {
   return (
     <div className="bg-primary w-full overflow-hidden">
+      {/* Navbar */}
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
           <Navbar />
